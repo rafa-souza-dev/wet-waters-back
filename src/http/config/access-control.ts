b0340a363ddc9a4dd552693f289069e490deb4d5
amadeusantos <amadeusantos31@gmail.com>
2023-10-06 21:27:45 -0300

    update in access control

--- src/http/config/access-control.ts
+++ src/http/config/access-control.ts
@@ -1,6 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { PrismaUsersRepository } from "../../repository/prisma/prisma-users-repository";
-import { EnumRole } from "@prisma/client";
+import { $Enums, EnumRole } from "@prisma/client";
 
 type MethodHTTP =
   | "GET"
@@ -28,11 +28,21 @@ export async function accessControl(
   request: FastifyRequest,
   response: FastifyReply
 ) {
+  let user: { id: number; username: string; github_id: string; role: $Enums.EnumRole; point: number; avatar_url: string | null; } | null;
+  if (request.routerPath !== "/api/auth/register") {
+    await request.jwtVerify();
+    const repository = new PrismaUsersRepository();
+    user = await repository.findById(Number(request.user.sub));
 
-  permissionAll("/api/auth/register");
-  permission("/api/v1/animals", ["ADMIN"], ["POST"]);
-  permission("/api/v2/animals", ["ADMIN"], ["POST"]);
+    if (!user) {
+      return response.send({ message: "User not found." }).status(401);
+    }
 
+    permission("/api/v1/animals", ["ADMIN"], ["POST"]);
+    permission("/api/v2/animals", ["ADMIN"], ["POST"]);
+    
+  }
+  
   function permission(
     route: string,
     roles: EnumRole[],
@@ -43,22 +53,9 @@ export async function accessControl(
     if (
       routeRegExp.test(request.routerPath) &&
       _methods.includes(request.routerMethod as MethodHTTP) &&
-      !roles.includes(request.user.role)
+      !roles.includes(user!.role)
     ) {
       return response.send({ message: "User not authorized." }).status(403);
-    }
-  }
-
-  async function permissionAll(route: string) {
-    const routeRegExp = new RegExp("^" + route.replaceAll("/**", "(/w)?"));
-    if (!routeRegExp.test(request.routerPath)) {
-      await request.jwtVerify();
-      const repository = new PrismaUsersRepository();
-      const user = await repository.findById(Number(request.user.sub));
-
-      if (!user) {
-        return response.send({ message: "User not found." }).status(401);
-      }
     }
   }
 };
